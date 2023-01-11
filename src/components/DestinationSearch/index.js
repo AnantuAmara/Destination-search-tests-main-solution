de88294e0fb4d143fc27b090a0db9ef19401e99e
root <amaraanantu@gmail.com>
2023-01-11 04:59:49 +0000

    Destination Search tests Solution

--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -6,15 +6,38 @@ import './index.css'
 import DestinationItem from '../DestinationItem'
 
 class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
   render() {
     const {destinationsList} = this.props
+
+    const {searchInput} = this.state
+    const searchResults = destinationsList.filter(each =>
+      each.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
 
     return (
       <div className="container">
         <h1 className="heading">Destination Search</h1>
-        <input type="search" />
+        <input
+          type="search"
+          placeholder="Search"
+          value={searchInput}
+          onChange={this.onChangeSearchInput}
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png"
+          alt="search icon"
+          className="search-icon"
+        />
         <ul className="list-container">
-          {destinationsList.map(eachUser => (
+          {searchResults.map(eachUser => (
             <DestinationItem destinationDetails={eachUser} key={eachUser.id} />
           ))}
         </ul>
